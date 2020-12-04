dabecbc73df63b7d3b622b7b9aa05f604ea6943c
barema4 <samrubarema6@gmail.com>
2020-12-04 23:39:15 +0300

    add initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBviSuMdUx97YBtOQ83VByOR4mR1pu7j3I",
+    authDomain: "fir-assignment-25f1a.firebaseapp.com",
+    databaseURL: "https://fir-assignment-25f1a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fir-assignment-25f1a",
+    storageBucket: "fir-assignment-25f1a.appspot.com",
+    messagingSenderId: "945540618254",
+    appId: "1:945540618254:web:bf63ba3a48ebeb47a7445f",
+    measurementId: "G-FSRD3375VQ"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBviSuMdUx97YBtOQ83VByOR4mR1pu7j3I",
+    authDomain: "fir-assignment-25f1a.firebaseapp.com",
+    databaseURL: "https://fir-assignment-25f1a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fir-assignment-25f1a",
+    storageBucket: "fir-assignment-25f1a.appspot.com",
+    messagingSenderId: "945540618254",
+    appId: "1:945540618254:web:bf63ba3a48ebeb47a7445f",
+    measurementId: "G-FSRD3375VQ"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
